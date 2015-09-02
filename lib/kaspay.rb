179cfb5a9d3f46e34b8fb1274475dbd43e58d7e4
Adrian Setyadi <a.styd@yahoo.com>
2015-09-02 07:33:13 +0700

    Add cookies handler; Remove kernel patch

--- lib/kaspay.rb
+++ lib/kaspay.rb
@@ -5,7 +5,6 @@
 require 'pstore'
 require 'watir'
 require 'headless'
-require_relative 'kernel_patch.rb'
 require_relative 'meta_stuff'
 
 class KasPay
@@ -17,6 +16,7 @@ class KasPay
    LOGIN_URL = BASE_URL + "/login" 
    DATA_DIR = ENV['HOME'] + "/.kaspay"
    LOGIN_PATH = DATA_DIR + "/login.dat"
+   COOKIE_PATH = DATA_DIR + "/cookie.dat"
 
    # Opening KasPay class singleton scope
    class << self
@@ -63,6 +63,39 @@ class KasPay
       def things_to_get
          all_get_methods.map{|m| m.id2name.sub("get_","")}
       end
+   
+      def data_scope path, &block
+         kasdb = PStore.new(path)
+         kasdb.transaction do
+            yield(kasdb)
+         end
+      end
+
+      def login_scope(&block)
+         data_scope(LOGIN_PATH, &block)
+      end
+    
+      def login_data_exists? login_name
+         data = nil
+         begin
+            login_scope {|login| data = login.roots}
+            return data.include? login_name
+         rescue NameError
+            return false
+         end
+      end
+   
+      def clear_login login_name = nil
+         login_scope do |login|
+            if !login_name.nil?
+               login.delete login_name
+            elsif login_name.nil?
+               login.roots.each do |name|
+                  login.delete name
+               end
+            end
+         end  
+      end
 
       def load_login login_name
          raise LoadLoginError, "login data \"#{login_name}\" cannot be found" unless login_data_exists? login_name
@@ -73,6 +106,11 @@ class KasPay
          end
          login email: email, password: password
       end
+   
+      def cookies_scope(&block)
+         data_scope(COOKIE_PATH, &block)
+      end
+       
  
       private :new
    end
@@ -86,6 +124,8 @@ class KasPay
    attr_reader :password
   
    # Some methods need to be inaccessible
+   private :browser
+   private :browser=
    private :headless
    private :headless=
    private :password
@@ -100,7 +140,7 @@ class KasPay
    end
    
    # Actually starts the login process after email and password
-   # provided. This is actualy different from the class method
+   # provided. This is different from the class method
    # `login` that only creates KasPay object and compounding the
    # data for login. 
    def login 
@@ -109,9 +149,25 @@ class KasPay
       
       @browser = Watir::Browser.start LOGIN_URL
 
-      browser.text_field(id: 'username').set email
-      browser.text_field(id: 'password').set password
-      browser.button(name: 'button').click
+      # Delete all expired cookies
+      delete_cookies_if_expire
+      # Use unexpired cookies that match the given email and password
+      the_cookies = use_cookies
+
+      # Fresh login
+      if the_cookies.nil?
+         browser.text_field(id: 'username').set email
+         browser.text_field(id: 'password').set password
+         browser.button(name: 'button').click
+         save_cookies
+      # Cookies still exist
+      else
+         browser.cookies.clear
+         the_cookies.each do |cookie|
+            browser.cookies.add(cookie[:name], cookie[:value])
+         end
+         browser.goto BASE_URL 
+      end
    end
 
    # Sets email input
@@ -124,6 +180,7 @@ class KasPay
    def password= pass 
       @password = pass
       login if user_data_complete?
+      return nil
    end
    
    def current_url
@@ -134,7 +191,7 @@ class KasPay
       url = (path[0] == "/" ? (BASE_URL + path) : path)
       browser.goto url
    end
-   
+
    def get_name
       browser.span(class: "user-name").text
    end
@@ -153,7 +210,9 @@ class KasPay
    end
 
    def logout
+      calling_method = caller[0][/`.*'/][1..-2]
       logout_link.click
+      delete_cookies unless calling_method == "delete_cookies"
    end
    
    def logout!
@@ -175,9 +234,10 @@ class KasPay
 
    def save_login login_name
       Dir.mkdir(DATA_DIR) unless Dir.exists?(DATA_DIR)
-      login_scope do |login|
+      KasPay.login_scope do |login|
          login[login_name] = {email: email, password: password}
       end
+      return nil
    end
 
    def inspect
@@ -200,7 +260,58 @@ class KasPay
    alias_method :url, :current_url
  
 private
-  
+   
+   def get_cookies
+      browser.cookies.to_a
+   end
+ 
+   def current_cookies_scope
+      KasPay.cookies_scope do |cookies|
+         cookies.roots.reverse.each do |cookies_name|
+            if cookies[cookies_name][:email] == email \
+                  && cookies[cookies_name][:password] == password
+               yield(cookies, cookies_name)
+            end
+         end
+      end
+   end
+   
+   def delete_cookies_if_expire 
+      current_cookies_scope do |cookies, cookies_name|
+         cookies[cookies_name][:cookies].each do |el|
+            if el[:name] == "kaspay_csrf_cookie" \
+                  && DateTime.parse(el[:expires].to_s) < DateTime.now
+               cookies.delete(cookies_name)
+            end
+         end
+      end
+   end
+
+   def delete_cookies 
+      current_cookies_scope do |cookies, cookies_name|
+         cookies.delete(cookies_name)
+      end
+      logout if logged_in?
+      return nil
+   end
+    
+   def save_cookies
+      time = Time.now
+      cookies_name = time.strftime("%y%m%d%H%M%S").to_i.to_s(36)
+      Dir.mkdir(DATA_DIR) unless Dir.exists?(DATA_DIR)
+      KasPay.cookies_scope do |cookie|
+         cookie[cookies_name] = {email: email, password: password, cookies: get_cookies}
+      end
+   end
+
+   def use_cookies
+      the_cookie = nil
+      current_cookies_scope do |cookies, cookies_name|
+         the_cookie = cookies[cookies_name][:cookies]
+      end
+      return the_cookie
+   end
+
    def logout_link
       logout_link = browser.a(href: "https://www.kaspay.com/account/logout")
    end
